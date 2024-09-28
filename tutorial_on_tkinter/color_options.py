6fbe8649ee86345eebabe0989fa53f9714ef9afb
fakoy <salamot.fakoya08@gmail.com>
2024-09-28 09:37:57 -0500

    day - 01 of text_to_audio

---
+++ tutorial_on_tkinter/color_options.py
@@ -0,0 +1,57 @@
+
+"""
+
+                Color Options:
+--> activebackground
+--> activeforeground
+--> background
+--> disabledforeground
+--> foreground
+--> highlightbackground
+--> highlightcolor
+--> selectbackrgound
+--> selectforeground
+
+            Fonts:
+--> Simple Tuple Fonts:
+        ("Helvetica", 16)
+        ("Times", 24, "bold italic")
+
+--> Font Object Fonts:
+        import tkFont
+        font = tkFont.Font(option) where option can be:
+            --> Family, Size, Weight, Slant, Underline, overstrike
+
+--> Anchors: are used to define where text is positioned relative to a reference point:
+        NW, N, NE, W, CENTER, E, SW, S, SE
+
+-->
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tutorial_on_tkinter/tkinter_widgets.py
+++ tutorial_on_tkinter/tkinter_widgets.py
@@ -2,9 +2,25 @@
 # This is a script on tkinter widgets
 
 """
-Tkinter
-
-
+                Widgets:
+1. Button
+2. Canvas
+3. Checkbutton
+4. Frame
+5. Label: to provide single line caption for other widgets and can contain images
+6. Listbox
+7. Menubutton
+8. Menu
+9. Message: to display multi-line fields to accept input from user
+9. Radiobutton
+10. Scale
+11. Scrollbar
+12. Text
+13. Toplevel
+14. Spinbox
+15. PanedWindow
+16. LabelFrame
+17. tkMessagebox
 """
 
 
