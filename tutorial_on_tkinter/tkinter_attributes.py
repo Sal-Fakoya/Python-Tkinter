6adf7bb86fe93e958ffbcdc53aff80664018b6a8
fakoy <salamot.fakoya08@gmail.com>
2024-09-28 08:43:04 -0500

    day - 01 of text_to_audio

---
+++ tutorial_on_tkinter/tkinter_attributes.py
@@ -0,0 +1,63 @@
+
+ # This is a script on tkinter attributes
+
+"""
+Attributes:
+1. Dimensions: 
+    in c (centimeters), i (inches), m (millimeter), p (printers point)
+    Length Options in pixels (px): borderwidth, highlightthickness, padx, pady, selectborderwidth, wraplength, 
+                                    height, underline, width
+2. Colors 
+3. Fonts
+4. Anchore
+5. Relief Styles
+6. Bitmaps
+7. Cursors
+ 
+ """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
